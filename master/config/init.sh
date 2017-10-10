07e440ca04ce27dd741482decbccb5086c72d6e7
pgmendez <pgmendez@sistem066984ln.bancocredicoop.coop>
2017-10-10 13:49:00 -0300

    add sshd

--- master/config/init.sh
+++ master/config/init.sh
@@ -1,120 +1,72 @@
 ## 
-
 ## Install the openssh-server and epel-release
-
 ##
 
-yum -y install openssh-server epel-release
-
+yum -y install openssh-server epel-release openssh-clients
 yum -y install pwgen
-
 rm -f /etc/ssh/ssh_host_ecdsa_key /etc/ssh/ssh_host_rsa_key
-
 ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_ecdsa_key
-
 ssh-keygen -q -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key 
-
 sed -i "s/#UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
-
 sed -i "s/UsePAM.*/UsePAM yes/g" /etc/ssh/sshd_config
-
 ssh-keygen -A
 
 ##
-
 ## Create Set Root Password Script. Name it as set_root_pw.sh. Save it in a folder
-
 ##
 
 #!/bin/bash
-
 if [ -f /.root_pw_set ]; then
-
-echo "Root password already set!"
-
-exit 0
-
+    echo "Root password already set!"
+    exit 0
 fi
 
+ROOT_PASS="Alfa1234"
 PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
-
 _word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
-
 echo "=> Setting a ${_word} password to the root user"
-
 echo "root:$PASS" | chpasswd
-
 echo "=> Done!"
-
 touch /.root_pw_set
-
 echo "========================================================================"
-
 echo "You can now connect to this CentOS container via SSH using:"
-
 echo ""
-
 echo "    ssh -p  root@"
-
 echo "and enter the root password '$PASS' when prompted"
-
 echo ""
-
 echo "Please remember to change the above password as soon as possible!"
-
 echo "========================================================================"
 
 ##
-
 ## Create run.sh file with following content and save it in same folder as the above
-
 ## set_root_pw.sh
-
 ##
 
 #!/bin/bash
 
 if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
-
     echo "=> Found authorized keys"
-
     mkdir -p /root/.ssh
-
     chmod 700 /root/.ssh
-
     touch /root/.ssh/authorized_keys
-
     chmod 600 /root/.ssh/authorized_keys
-
     IFS='\n'
-
     arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
-
     for x in $arr
-
     do
-
         x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
-
         cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
-
         if [ $? -ne 0 ]; then
-
             echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
-
             echo "$x" >> /root/.ssh/authorized_keys
-
         fi
-
     done
-
 fi
 
 if [ ! -f /.root_pw_set ]; then
-
-/set_root_pw.sh
-
+    /set_root_pw.sh
 fi
-
+# Start supervisor
+mkdir -p /var/log/supervisor
 supervisord -c /etc/supervisor/supervisord.conf
 #exec /usr/sbin/sshd -D
--- master/master.dockerfile
+++ master/master.dockerfile
@@ -2,7 +2,14 @@ FROM centos:latest
 
 WORKDIR /tmp
 
-RUN yum install python-setuptools && easy_install supervisor
+RUN export http_proxy="http://pgmendez:Octubre2017@10.1.1.88:3128" && \
+	export https_proxy="http://pgmendez:Octubre2017@10.1.1.88:3128" && \
+	echo "proxy=http://10.1.1.88:3128" >> /etc/yum.conf && \
+	echo "proxy_username=pgmendez" >> /etc/yum.conf && \
+	echo "proxy_password=Octubre2017" >> /etc/yum.conf && \
+	yum update -y && \
+	yum install python-setuptools -y && easy_install pip && \
+	pip install --proxy="$http_proxy" supervisor
 
 # Configure supervisor to start sshd
 ADD config/supervisor /tmp/supervisor
@@ -10,8 +17,8 @@ RUN mkdir -p /etc/supervisor/conf.d && \
     cp supervisor/supervisord.conf /etc/supervisor/supervisord.conf && \
     cp supervisor/sshd.conf /etc/supervisor/conf.d/
 
+# Configure ssh
 ADD config/init.sh /root/
+RUN chmod 777 /root/init.sh && /root/init.sh
 
-RUN touch /root/a.txt && chmod 777 /root/init.sh && /root/init.sh
-
-CMD tail -f /root/a.txt
+RUN tail -f /dev/null
